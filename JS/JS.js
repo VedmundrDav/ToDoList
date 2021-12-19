98f3928c97152e3e9e5f3fd0939dc97835ff54bf
Daniel <iban1031@gmail.com>
2021-12-19 13:21:54 -0600

    Added js to delete the item, added some logic to check if the input field is blank, added placeholder text for input field.

--- JS/JS.js
+++ JS/JS.js
@@ -3,14 +3,29 @@ var addBtn = document.getElementById("item-button");
 var list = document.getElementById("list");
 var item = document.getElementsByTagName("li");
  
-function createItem(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(inputText.value));
-    list.appendChild(li);
-    inputText.value = "";
+function createItem() {
+    if (!inputText.value == "") {
+        var delBtn = document.createElement("button");
+        delBtn.innerHTML = "X"
+        delBtn.id = "del-btn";
+        var li = document.createElement("li"); //creates a li element
+        li.appendChild(delBtn);
+        li.appendChild(document.createTextNode(inputText.value)); //sets text of input ot the li element
+
+
+        list.appendChild(li); //adds li element to the ul
+        inputText.value = ""; //resets the input field
+
+        delBtn.onclick = function () {
+            //delete the li
+            list.removeChild(li);
+        }
+    }else{
+        alert("Enter something to do!");
+    }
+
+    //need to add a button that deletes the row too
+
 }
 
-function addItem(){
-
-}
 
